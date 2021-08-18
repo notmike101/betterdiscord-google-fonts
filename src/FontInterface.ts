d411a0dde131e06ed6045d488d01a0a40875bde2
Mike Orozco <me@mikeorozco.dev>
2021-08-18 15:59:39 -0700

    Initial version

---
+++ src/FontInterface.ts
@@ -0,0 +1,12 @@
+export default interface Font {
+  family: String;
+  variants: String[];
+  subsets: String[];
+  version: String;
+  lastModified: String;
+  files: {
+    [key: string]: String;
+  };
+  category: String;
+  kind: String;
+}
