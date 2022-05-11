d5aeb5e7d0e290c0629c60989e3eed9a5d0342e1
Mike Orozco <me@mikeorozco.dev>
2022-05-10 19:50:15 -0700

    [WIP] Initial attempt

---
+++ .dev/update-google-fonts.mjs
@@ -0,0 +1,23 @@
+import fs from 'fs/promises';
+
+async function getRemoteFonts() {
+  const response = await fetch(
+    'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyCfxANDwWkAP3JjzGLv5UOPYcPEwaVAz3k'
+  );
+
+  const fonts = await response.json();
+
+  return fonts;
+}
+
+async function main() {
+  const fonts = await getRemoteFonts();
+
+  const content = JSON.stringify(fonts, null, 2);
+
+  await fs.writeFile('./src/google-fonts.json', content);
+
+  return;
+}
+
+main();
