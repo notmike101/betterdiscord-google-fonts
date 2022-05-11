4d2c6392a7198965d80e1888517ded8a49d07f97
Mike <7198316+notmike101@users.noreply.github.com>
2022-05-10 19:07:06 -0700

    Merge develop into main (#15)
    * Save original font and revert on deactivation
    
    * Allow build to work with newer NodeJS versions
    
    * Improve UI and functionality for settings panel
    
    * Improve metadata of plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,24 @@ import pkg from './package.json';
 import path from 'path';
 
 const metaComment = {
-  name: pkg.name,
-  version: pkg.version,
-  description: pkg.description,
-  author: pkg.author?.name ?? pkg.author,
-  authorLink: pkg.author?.url ?? pkg.homepage,
-  authorId: pkg.betterdiscord?.discord_snowflake,
+  name: pkg.betterdiscord?.plugin_name ?? pkg.name,
+  version: pkg.version ?? '1.0.0',
+  description: pkg.description ?? '',
+  author: pkg.betterdiscord?.author_name ?? pkg.author?.name ?? pkg.author,
+  website: pkg.author?.url ?? pkg.homepage ?? '',
+  authorLink: pkg.author?.url ?? pkg.homepage ?? '',
+  authorId: pkg.betterdiscord?.discord_snowflake ?? '',
   updateUrl: pkg.betterdiscord?.update_url,
   source: pkg.betterdiscord?.source,
 };
 
 export default defineConfig(({ mode }) => ({
   plugins: [
-    banner(Object.entries(metaComment).reduce((acc, [key, value]) => `${acc}\n * @${key} ${value}`, '/**\n') + '\n */\n'),
+    banner(
+      Object.entries(metaComment)
+        .filter(([key, value]) => value !== '' && value !== null && value !== undefined)
+        .reduce((acc, [key, value]) => `${acc}\n * @${key} ${value}`, '/**\n') + '\n */\n'
+    ),
   ],
   resolve: {
     alias: {
