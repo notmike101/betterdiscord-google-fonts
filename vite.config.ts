bad6ca7b8cc7467692c2eaa9afe7a1c1400030c4
Mike Orozco <me@mikeorozco.dev>
2022-05-10 19:04:22 -0700

    Improve metadata

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,23 @@ import path from 'path';
 
 const metaComment = {
   name: pkg.betterdiscord?.plugin_name ?? pkg.name,
-  version: pkg.version,
-  description: pkg.description,
-  author: pkg.author?.name ?? pkg.author,
-  authorLink: pkg.author?.url ?? pkg.homepage,
-  authorId: pkg.betterdiscord?.discord_snowflake,
+  version: pkg.version ?? '1.0.0',
+  description: pkg.description ?? '',
+  author: pkg.betterdiscord?.author_name ?? pkg.author?.name ?? pkg.author,
+  website: pkg.author?.url ?? pkg.homepage ?? '',
+  authorLink: pkg.author?.url ?? pkg.homepage ?? '',
+  authorId: pkg.betterdiscord?.discord_snowflake ?? '',
   updateUrl: pkg.betterdiscord?.update_url,
   source: pkg.betterdiscord?.source,
 };
 
 export default defineConfig(({ mode }) => ({
   plugins: [
-    banner(Object.entries(metaComment).reduce((acc, [key, value]) => `${acc}\n * @${key} ${value}`, '/**\n') + '\n */\n'),
+    banner(
+      Object.entries(metaComment)
+        .filter(([key, value]) => value !== '' && value !== null && value !== undefined)
+        .reduce((acc, [key, value]) => `${acc}\n * @${key} ${value}`, '/**\n') + '\n */\n'
+    ),
   ],
   resolve: {
     alias: {
