9d1eeeb8d6173cf4e014ca96ca839947b4954860
Mike Orozco <me@mikeorozco.dev>
2022-05-24 13:56:14 -0700

    Use version & description from package.json

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,6 +1,9 @@
 import esbuild from 'esbuild';
 import { sassPlugin } from 'esbuild-sass-plugin';
 import { metaInfoAsJSComment } from '../betterdiscord.config.mjs';
+import fs from 'fs';
+
+const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
 async function main() {
   await esbuild.build({
@@ -10,6 +13,10 @@ async function main() {
     watch: process.argv.slice(2).includes('--watch'),
     banner: {
       js: metaInfoAsJSComment(),
+    },
+    define: {
+      'process.env.VERSION': JSON.stringify(packageJson.version),
+      'process.env.DESCRIPTION': JSON.stringify(packageJson.description),
     },
     entryPoints: ['./src/main.tsx'],
     outfile: './dist/betterdiscord-google-fonts.plugin.js',
